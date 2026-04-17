fab0c2769bfc9a91bcc8a84ccce8e4bb9bb40a97
cairo387 <mifuneri@outlook.jp>
2026-04-17 16:46:04 +0900

    担当者別集計機能追加

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from config import INPUT_CSV_PATH
 def main():
     result = analyze_testcase(INPUT_CSV_PATH)
 
-    print("=== テストケース分析結果 ===")
-    for key,value in result.items():
+    print("=== サマリー ===")
+    for key,value in result["サマリー"].items():
         print(f"{key}: {value}")
+
+    print("\n=== 担当者別集計 ===")
+    print(result["担当者別集計"])
 
 if __name__ == "__main__":
     main()
--- src/csv_analyzer.py
+++ src/csv_analyzer.py
@@ -18,14 +18,25 @@ def analyze_testcase(csv_path: str) -> dict:
     execution_rate = executed_count / total_count if total_count else 0
     success_rate = ok_count / executed_count if ok_count else 0
 
+    # 担当者別集計
+    # df.groupby("担当者")["結果"]：dateframeを担当者ごとに分け結果列だけを参照
+    # value_counts()：出現回数をカウント
+    # unstack(fill_value=0)：視認性が悪いためMultiIndex形式をクロス集計表形式に変換
+    person_summary = (
+        df.groupby("担当者")["結果"].value_counts().unstack(fill_value=0)
+    )
+
     reslut = {
-        "総件数": total_count,
-        "OK件数": ok_count,
-        "NG件数": ng_count,
-        "未実施件数": not_executed_count,
-        "実施済み件数": executed_count,
-        "実施率": round(execution_rate, 2),
-        "成功率": round(success_rate, 2),
+        "サマリー":{
+            "総件数": total_count,
+            "OK件数": ok_count,
+            "NG件数": ng_count,
+            "未実施件数": not_executed_count,
+            "実施済み件数": executed_count,
+            "実施率": round(execution_rate, 2),
+            "成功率": round(success_rate, 2),
+        },
+        "担当者別集計": person_summary,
     }
 
     return reslut
